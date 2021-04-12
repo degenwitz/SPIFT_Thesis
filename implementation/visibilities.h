567cbce259f64f1ee37971968226da8401162c87
Degenwitz <dimitri.degkwitz@uzh.ch>
2021-04-12 10:20:01 +0200

    draft for local implementation

--- implementation/visibilities.h
+++ implementation/visibilities.h
@@ -12,18 +12,18 @@ public:
     visibility(std::complex<double> vis,int u,int v): u(u), v(v), vis(vis) { }
 
     void calc_isCS(){
-        isCS = (v == 0) || (u%2==1 && v%2 == 0) || (std::__gcd(u,N) < std::__gcd(v,N) );
+        isCS = (v == 0) || (u%2==1 && v%2 == 0) || (v%2 == 0 && std::__gcd(u,N) < std::__gcd(v,N) );
     }
 
     void calc_shift_index(){
-        if( u == 0 or v == 0) shift_index = 0;
+        if( u == 0 || v == 0) {shift_index = 0; return; }
         if(isCS){
             for( int j = 0; j < N; ++j){
-                if(v==(j*u)%N) shift_index = j;
+                if(v==(j*u)%N){ shift_index = j; return; }
             }
         } else {
             for( int k = 0; k < N; ++k){
-                if(u == (k*v)%N) shift_index = k;
+                if(u == (k*v)%N){ shift_index = k; return; }
             }
         }
     }
